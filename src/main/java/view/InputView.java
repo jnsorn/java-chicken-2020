67ed1fa4dee8aaac3199992fcd37ace8fbd1152e
soring <jnsorn@gmail.com>
2020-04-10 14:59:34 +0900

    feat : 메뉴, 수량을 입력 받는 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,13 +3,28 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static String inputAction(){
-        return scanner.nextLine();
-    }
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
-    }
+	public static String inputAction() {
+		System.out.println("## 메인화면");
+		System.out.println("1 - 주문등록");
+		System.out.println("2 - 결제하기");
+		System.out.println("3 - 프로그램 종료");
+		return scanner.nextLine();
+	}
+
+	public static int inputTableNumber() {
+		System.out.println("## 주문할 테이블을 선택하세요.");
+		return scanner.nextInt();
+	}
+
+	public static String inputMenu() {
+		System.out.println("## 등록할 메뉴를 선택하세요.");
+		return scanner.nextLine();
+	}
+
+	public static String inputQuantity() {
+		System.out.println("## 메뉴의 수량을 입력하세요.");
+		return scanner.nextLine();
+	}
 }
