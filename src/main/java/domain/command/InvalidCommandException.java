6124ddb86a6238447f3ce221b2f4110ba7c6f672
soring <jnsorn@gmail.com>
2020-04-10 14:58:38 +0900

    refactor : 패키지 분리

--- src/main/java/domain/command/InvalidCommandException.java
+++ src/main/java/domain/command/InvalidCommandException.java
@@ -1,7 +1,7 @@
-package domain;
+package domain.command;
 
 public class InvalidCommandException extends IllegalArgumentException {
-	public static final String INVALID_COMMAND = "유효하지 않은 명령어입니다.";
+	static final String INVALID_COMMAND = "유효하지 않은 명령어입니다.";
 
 	public InvalidCommandException(String s) {
 		super(s);
